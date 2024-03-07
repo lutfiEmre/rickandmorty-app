93bd8196760ea35a6f7d715581a8375751d373e3
Emre Lutfi <mail@emrelutfi.com>
2024-03-07 21:00:11 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
-import {Inter, Montserrat} from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import "./main.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const montserrat = Montserrat({ subsets: ["latin"] });
 
 
 export const metadata: Metadata = {
